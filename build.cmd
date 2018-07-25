490e57fdb84f845ac4e1e6aef82f6822e65a21fb
Andre Soares <andregsilv@gmail.com>
2018-07-25 11:05:37 -0300

    Skip cleanup

--- build.cmd
+++ build.cmd
@@ -1,10 +1,18 @@
 set Configuration=Release
 set OutputDir=artifacts\
 
+rmdir /s/q src\Sclertta.Databases.Accounts\bin
+rmdir /s/q src\Sclertta.Databases.Applications\bin
+rmdir /s/q src\Sclertta.Databases.Personnel\bin
+rmdir /s/q src\Sclertta.Databases.Persons\bin
+rmdir /s/q src\Sclertta.Databases.Rbac\bin
+rmdir /s/q src\Sclertta.Databases.State\bin
+rmdir /s/q src\Sclertta.Databases.Users\bin
+
 dotnet build -c %Configuration% src\Sclertta.Databases.sln
 dotnet pack  -c %Configuration% src\Sclertta.Databases.sln
 
-if not exist %OutputDir% mkdir %OutputDir%
+if exist %OutputDir% rmdir /s/q %OutputDir%
 
 copy src\Sclertta.Databases.Accounts\bin\%Configuration%\*.nupkg %OutputDir%
 copy src\Sclertta.Databases.Applications\bin\%Configuration%\*.nupkg %OutputDir%
--- build.sh
+++ build.sh
@@ -3,9 +3,19 @@
 Configuration=Release
 OutputDir=artifacts/
 
+rm -rf src/Sclertta.Databases.Accounts/bin
+rm -rf src/Sclertta.Databases.Applications/bin
+rm -rf src/Sclertta.Databases.Personnel/bin
+rm -rf src/Sclertta.Databases.Persons/bin
+rm -rf src/Sclertta.Databases.Rbac/bin
+rm -rf src/Sclertta.Databases.State/bin
+rm -rf src/Sclertta.Databases.Users/bin
+
+
 dotnet build -c $Configuration src/Sclertta.Databases.sln
 dotnet pack  -c $Configuration src/Sclertta.Databases.sln
 
+rm -rf $OutputDir
 mkdir -p $OutputDir
 
 cp src/Sclertta.Databases.Accounts/bin/$Configuration/*.nupkg $OutputDir
