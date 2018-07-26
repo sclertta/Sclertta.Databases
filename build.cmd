bb014a425f7465b70fd86d4e449f61135c49d595
Andre Soares <andregsilv@gmail.com>
2018-07-26 10:33:22 -0300

    better scripts

--- build.cmd
+++ build.cmd
@@ -1,6 +1,10 @@
+@echo off
+
+REM Set variables
 set Configuration=Release
 set OutputDir=artifacts\
 
+REM Remove bin/ folders
 rmdir /s/q src\Sclertta.Databases.Accounts\bin
 rmdir /s/q src\Sclertta.Databases.Applications\bin
 rmdir /s/q src\Sclertta.Databases.Personnel\bin
@@ -9,11 +13,13 @@ rmdir /s/q src\Sclertta.Databases.Rbac\bin
 rmdir /s/q src\Sclertta.Databases.State\bin
 rmdir /s/q src\Sclertta.Databases.Users\bin
 
-dotnet build -c %Configuration% src\Sclertta.Databases.sln
-dotnet pack  -c %Configuration% src\Sclertta.Databases.sln
+dotnet pack -c %Configuration% src\Sclertta.Databases.sln
 
+REM Delete OutputDir
 if exist %OutputDir% rmdir /s/q %OutputDir%
+mkdir $OutputDir
 
+REM Copy nuget packages to OutputDir
 copy src\Sclertta.Databases.Accounts\bin\%Configuration%\*.nupkg %OutputDir%
 copy src\Sclertta.Databases.Applications\bin\%Configuration%\*.nupkg %OutputDir%
 copy src\Sclertta.Databases.Personnel\bin\%Configuration%\*.nupkg %OutputDir%
--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Set variables
 Configuration=Release
 OutputDir=artifacts/
 
+# Remove bin/ folders
 rm -rf src/Sclertta.Databases.Accounts/bin
 rm -rf src/Sclertta.Databases.Applications/bin
 rm -rf src/Sclertta.Databases.Personnel/bin
@@ -11,13 +13,13 @@ rm -rf src/Sclertta.Databases.Rbac/bin
 rm -rf src/Sclertta.Databases.State/bin
 rm -rf src/Sclertta.Databases.Users/bin
 
-
-dotnet build -c $Configuration src/Sclertta.Databases.sln
 dotnet pack  -c $Configuration src/Sclertta.Databases.sln
 
+# Delete OutputDir
 rm -rf $OutputDir
 mkdir -p $OutputDir
 
+# Copy nuget packages to OutputDir
 cp src/Sclertta.Databases.Accounts/bin/$Configuration/*.nupkg $OutputDir
 cp src/Sclertta.Databases.Applications/bin/$Configuration/*.nupkg $OutputDir
 cp src/Sclertta.Databases.Personnel/bin/$Configuration/*.nupkg $OutputDir
